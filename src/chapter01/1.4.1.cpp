9a2a65de0aec02b9358a643c4df63e6cdf87c34f
hushicai <bluthcy@gmail.comm>
2019-09-23 13:43:16 +0800

    1.4.1

---
+++ src/chapter01/1.4.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, val = 1;
+    while (val <= 10)
+    {
+        sum += val;
+        ++val;
+    }
+    std::cout << "Sum of 1 to 1- inclusive is "
+        << sum << std::endl;
+    return 0;
+}
